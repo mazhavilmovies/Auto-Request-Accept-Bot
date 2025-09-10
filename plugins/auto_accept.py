96555d1f84b64e93e9d0bf497b633593821e0f7a
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-09-10 23:55:50 +0600

    Update auto_accept.py

--- plugins/auto_accept.py
+++ plugins/auto_accept.py
@@ -1,17 +1,18 @@
 from bot import Bot 
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ChatPermissions, Message, ChatJoinRequest 
+from pyrogram.enums import ChatAction
 from config import LOGGER, OWNER_ID
 logger = LOGGER("auto_accept.py")
 
 
 
 @Bot.on_chat_join_request(filters.group | filters.channel)
-async def auto_accept(client: Bot, message: ChatJoinRequest):
+async def auto_accept(client: Client, message: ChatJoinRequest):
     chat, user = message.chat, message.from_user
     
     # remove those two when public the repo
-    LOGGER.info(f"{'@' + user.username if user.username else user.user_id} Joined {chat.title} in {chat.id} with id: {user.id}") 
+    logger.info(f"{'@' + user.username if user.username else user.user_id} Joined {chat.title} in {chat.id} with id: {user.id}") 
     await client.send_message(chat_id=OWNER_ID, text=f"{user.mention}!\n\n Joined {chat.title} ID:{chat.id} USER ID:{user.id}")
     # accept request
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
@@ -25,13 +26,12 @@ async def auto_accept(client: Bot, message: ChatJoinRequest):
     
     
     # channel buttons
-    buttons = None
-    if invite_link:
-      buttons = InlineKeyboardMarkup([
-        [
-          InlineKeyboardButton("🧭 Vɪsɪᴛ Vʜᴀɴɴᴇʟ", url=invite_link)
-        ]
-      ]) if invite_link else None
+    
+    buttons = InlineKeyboardMarkup([
+      [
+        InlineKeyboardButton("🧭 Vɪsɪᴛ Vʜᴀɴɴᴇʟ", url=invite_link)
+      ]
+    ]) if invite_link else None
     
     
     await client.send_message(
