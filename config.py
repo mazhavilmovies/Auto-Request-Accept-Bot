990525a78cac5535753f0c40a5378759d24c983c
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-10-20 12:52:12 +0600

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,29 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5296584067"))
 PORT = os.environ.get("PORT", "8080")
 DB_URL = os.environ.get("DB_URL", "#db url here")
 DB_NAME = os.environ.get("DB_NAME", "JOINREQ")
-START_MSG = os.environ.get("START_MSG", "<blockquote>Hello {mention}</blockquote>\n\n<b>I 'm a bot who can steal videos from youtube and give it to you but i am exoecting rewards from you! :)</b>")
-ABOUT_MSG = os.environ.get("ABOUT_MSG", "<b><blockquote>About us</blockquote></b>\n\nCreator: <a href='https://t.me/OnlyNoco'></a>\nPortfolio: <a href='href='https://onlynoco.vercel.app'></a>\n\n <blockquote>Channels,</blockquote>\n<blockquote><a href='https://t.me/HeavenlySubs'>Donghua</a>\n<a href='https://t.me/+O7PeEMZOAoMzYzVl'>Hentai</a></blockquote>")
-CMD_MSG = os.environ.get("CMD_MSG", "<blockquote>/start - to check bot alive or dead!\n/help - to get help from bot usuages\n/report - to report issue to admin")
+START_MSG = os.environ.get(
+    "START_MSG",
+    "<blockquote>Hello {mention}</blockquote>\n\n"
+    "<b>I watch all that I am permitted to see. "
+    "With the right access, I let anyone join private channels instantly. "
+    "Add me as admin, and you can rely on me.</b>"
+)
+ABOUT_MSG = """
+<b>›› ᴀʙᴏᴜᴛ ᴍᴇ:</b>
+◧ ᴏᴡɴᴇʀ: <a href='https://t.me/OnlyNoco'>ᴏɴʟʏɴᴏᴄᴏ</a>
+◧ ᴘᴏʀᴛғᴏʟɪᴏ: <a href='https://onlynoco.vercel.app'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
+◧ ʀᴇᴘᴏ: <a href='https://github.com/OnlyNoco/Auto-Request-Accept-Bot'>ɢɪᴛʜᴜʙ</a>
+◧ ʟɪʙs ~ 
+<a href='https://github.com/Mayuri-Chan/pyrofork'>ᴘʏʀᴏғᴏʀᴋ</a> • 
+<a href='https://www.python.org/'>ᴘʏᴛʜᴏɴ</a> • 
+<a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏᴅʙ</a>
+
+◧ ᴏᴜʀ sᴇʀᴠɪᴄᴇs ~
+⊡ <a href='https://t.me/+O7PeEMZOAoMzYzVl'>ʜᴇɴᴛᴀɪ ᴄʀɪsᴘ</a>
+⊡ <a href='https://t.me/HeavenlySubs'>ʙᴀᴛᴛʟᴇ ᴛʜʀᴏᴜɢʜ ᴛʜᴇ ʜᴇᴀᴠᴇɴs</a>
+⊡ <a href='https://t.me/CrispAnime'>ᴄʀɪsᴘ ᴀɴɪᴍᴇ</a>
+"""
+CMD_MSG = os.environ.get("CMD_MSG", "<blockquote>/start - to check bot alive or dead!\n/help - to get help from bot usuages\n/report - to report issue to admin\nSend or Forward anything i will broadcast it to all users.")
 START_PIC = os.environ.get("START_PIC", "https://envs.sh/bjb.mp4 https://envs.sh/bjP.mp4 https://envs.sh/bjw.mp4 https://envs.sh/bj0.mp4 https://envs.sh/bjS.mp4 https://envs.sh/bjW.mp4 https://envs.sh/bjB.mp4 https://envs.sh/bjI.mp4 https://envs.sh/bjn.mp4 https://envs.sh/bjT.mp4 https://envs.sh/bjZ.mp4 https://envs.sh/bjL.mp4 https://envs.sh/bj5.mp4 https://envs.sh/bjY.mp4 https://envs.sh/bjC.mp4").split(" ")
 FLOOD_WAIT = int(os.environ.get("FLOOD_WAIT", "10")) # in seconds
 
