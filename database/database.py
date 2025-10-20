c77f224fe74d5e37a234e58980ab6d438a92e2e1
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-10-20 10:11:21 +0600

    Create database.py
    # added database mongodb

---
+++ database/database.py
@@ -0,0 +1,23 @@
+from config import DB_URL, DB_NAME
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+# clients setup 
+_client = AsyncIOMotorClient(DB_URL) 
+_db = _client[DB_NAME] 
+_users = _db.users # collection name "users"
+
+# save id 
+async def add_user(user_id: int):
+  await _users.update_one(
+    {"_id": user_id}, {"$setOnInsert": {"_id": user_id}}, upsert=True
+  )
+  
+# get all users 
+async def get_all_users() -> list[int]:
+  curser = _users.find({}, {_id: 1}) 
+  return [doc["_id"] async for doc in curser] 
+  
+# delete users [bot blocked or deactivated account cleanup] 
+async def del_user(user_id: int):
+  await _users.delete_one({"_id": user_id}) 
